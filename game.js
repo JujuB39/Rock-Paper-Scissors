547e379f30a5437697f2c0a53abd4ce48e50d3f8
Julissa Bonilla <julissab9@gmail.com>
2022-09-12 19:37:33 -0500

    psuedo-code

--- game.js
+++ game.js
@@ -1,1 +1,21 @@
-console.log("yo");
+// define array for choices [r, p, s]
+// define var for computerChoice 
+// define var for playerChoice
+// define var for win 
+// define var for losses 
+// define var for ties
+
+// prompt playerChoice
+
+// randomly choose computerChoice
+
+// compare choices if else
+
+// display (alert) results (won, tied, or lose)
+
+//stats (number of win, losses, or ties)
+
+// ask to play again ?
+    // restart the game 
+//else 
+    // end the game 
